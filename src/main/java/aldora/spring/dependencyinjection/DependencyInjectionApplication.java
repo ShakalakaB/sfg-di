e61ec980ebd49884767af56a2558a12738862f5f
ShakalakaB <yunjie989@gmail.com>
2021-05-05 17:59:43 +0800

    Get controller from Spring context

--- src/main/java/aldora/spring/dependencyinjection/DependencyInjectionApplication.java
+++ src/main/java/aldora/spring/dependencyinjection/DependencyInjectionApplication.java
@@ -1,13 +1,21 @@
 package aldora.spring.dependencyinjection;
 
+import aldora.spring.dependencyinjection.controllers.MyController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class DependencyInjectionApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DependencyInjectionApplication.class, args);
+		ApplicationContext applicationContext = SpringApplication.run(DependencyInjectionApplication.class, args);
+
+		MyController myController = (MyController) applicationContext.getBean("myController");
+
+		String greeting = myController.sayHello();
+
+		System.out.println(greeting);
 	}
 
 }
---
+++ src/main/java/aldora/spring/dependencyinjection/controllers/MyController.java
@@ -0,0 +1,12 @@
+package aldora.spring.dependencyinjection.controllers;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class MyController {
+    public String sayHello() {
+        System.out.println("hello world");
+
+        return "hello, folks";
+    }
+}
